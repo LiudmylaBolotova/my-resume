5128547d7a1cc0f0314ab3dad74a80b37430f078
Liudmyla Bolotova <89790016+LiudmylaBolotova@users.noreply.github.com>
2023-10-23 21:51:27 +0300

    Update Education.jsx

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -11,8 +11,9 @@ const Education = () => {
       <TertiaryTitle>Education</TertiaryTitle>
       <SecondaryText>Full stack developer</SecondaryText>
       <Text>IT academy GoIT, Ukraine </Text>
-      <Text>september 2021 - today</Text>
+      <Text>september 2021 - october 2023</Text>
     </StyledSection>
+    
   );
 };
 
